cf1e044866c9de6c5b46fe92568a8790ffbc6c4c
Omer-KISAKOL <72518514+Omer-KISAKOL@users.noreply.github.com>
2021-05-31 00:43:30 +0300

    Add files via upload

---
+++ yz.randomHist.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+np.random.seed(0)
+values = np.random.randn(100) 
+s = pd.Series(values)
+
+s.plot(kind = 'hist', title = 'Normally distributed random values') 
+
+plt.show()
+
+print(s.describe())
