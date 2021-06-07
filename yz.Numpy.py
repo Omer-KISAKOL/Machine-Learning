63bed47c6e184a67dd52e216d2368a0163f92ef4
ÖMER KISAKOL <okisakol@gmail.com>
2021-06-07 21:34:41 +0300

    Add files via upload

---
+++ yz.Numpy.py
@@ -0,0 +1,12 @@
+#Numpy
+import numpy as np
+
+x = np.array([1,2,3,4])
+#x = np.arange(4)
+#x = np.zeros((3,2))
+#x = np.ones((3,2))
+#x = np.linspace(0,1,21)
+#x = np.logspace(-2,2,5)
+#x = np.fromfunction(lambda i: i**2, (5,))
+
+print(x,'\n')
