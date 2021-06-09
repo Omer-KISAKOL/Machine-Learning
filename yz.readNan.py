6db7572232b2de62b14bc474f5647315551d2035
ÖMER KISAKOL <okisakol@gmail.com>
2021-06-09 20:29:45 +0300

    Add files via upload

---
+++ yz.readNan.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+np.random.seed(145)
+
+veriSeti = pd.read_csv('SISA.csv')
+
+#print(veriSeti.head(3))
+
+#Veri setindeki 'nan' değerlerini değiştirmek için:
+veriSeti.fillna(0, inplace = True)
+
+features = veriSeti.iloc[:,2:36]
+
+classes = veriSeti.Hospitalized
