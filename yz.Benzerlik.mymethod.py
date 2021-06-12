0db0f1df86d63e76c3ab3f61cdd90646c4cd9360
ÖMER KISAKOL <okisakol@gmail.com>
2021-06-12 10:25:49 +0300

    Add files via upload

---
+++ yz.Benzerlik.mymethod.py
@@ -0,0 +1,12 @@
+from similarity_measures import Benzerlik
+
+def main():
+    measures = Benzerlik()
+    
+    x = [5,6,7,9,4]
+    y = [1,8,9,2,3]
+    
+    print(measures.oklit(x, y))
+    
+if __name__ == '__main__':
+    main()
