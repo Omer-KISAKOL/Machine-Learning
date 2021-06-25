8b61ad4c0d6a40304ffaa2cf9cfb3290f818f577
ÖMER KISAKOL <okisakol@gmail.com>
2021-06-25 14:53:56 +0300

    Update yz.ilkGrafik.py

--- yz.ilkGrafik.py
+++ yz.ilkGrafik.py
@@ -5,6 +5,7 @@ plt.axis('auto')
 y1=[120,122,124,126]
 y2=[100,102,103,104]
 X=[datetime.datetime.now()+datetime.timedelta(minutes=i) for i in range(len(y1))]
+
 plt.plot(X,y1,'sr')
 plt.plot(X,y2,'m*')
 plt.legend(['Makina1', 'Makina2'])
