7ff464b97b6c17fec0a4f96d69a32a13b32f3d3b
ÖMER KISAKOL <okisakol@gmail.com>
2021-07-07 00:09:18 +0300

    Update yz.DataFrame.append.py

--- yz.DataFrame.append.py
+++ yz.DataFrame.append.py
@@ -5,7 +5,6 @@ vs1 = pd.DataFrame(columns = ['A','B'])
 vs1.loc[0] = {'A':'a1','B':'b1'}
 vs1.loc[1] = {'A':'a2','B':'b2'} 
 
-
 vs2 = pd.DataFrame(columns = ['B','C'])
 
 vs2.loc[0] = {'B':'b1', 'C':'c1'}
